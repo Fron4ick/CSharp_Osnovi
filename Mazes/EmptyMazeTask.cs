4281f4486ad990801bce73f11070be0939db0b50
Fron <f_rozhin123@mail.ru>
2025-10-17 16:27:30 +0500

    Mazes

--- Mazes/EmptyMazeTask.cs
+++ Mazes/EmptyMazeTask.cs
@@ -2,8 +2,24 @@
 
 public static class EmptyMazeTask
 {
-	public static void MoveOut(Robot robot, int width, int height)
-	{
-		
-	}
+    public static void MoveOut(Robot robot, int width, int height)
+    {
+        MoveDown(robot, height - 2);
+        MoveRight(robot, width - 2);
+    }
+    public static void MoveDown(Robot robot, int steps)
+    {
+        for (int i = 1; i < steps; i++)
+        {
+            robot.MoveTo(Direction.Down);
+        }
+    }
+
+    public static void MoveRight(Robot robot, int steps)
+    {
+        for (int i = 1; i < steps; i++)
+        {
+            robot.MoveTo(Direction.Right);
+        }
+    }
 }
