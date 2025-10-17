7582d295822f9ac51db32dcc923d32a89493e6b8
Fron <f_rozhin123@mail.ru>
2025-10-17 18:28:32 +0500

    SnakeMazes last code

--- Mazes/SnakeMazeTask.cs
+++ Mazes/SnakeMazeTask.cs
@@ -4,6 +4,36 @@ public static class SnakeMazeTask
 {
     public static void MoveOut(Robot robot, int width, int height)
     {
+        while (robot.Y != height - 2)
+        {
+            MoveDownRightDown(robot, width - 2);
+            MoveLeft(robot);
+        }
+	}
 
+    public static void MoveDownTwo(Robot robot)
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            robot.MoveTo(Direction.Down);
+        }
+    }
+
+    public static void MoveDownRightDown(Robot robot, int width)
+    {
+        if (robot.Y != 1) MoveDownTwo(robot);
+        while (robot.X != width)
+        {
+            robot.MoveTo(Direction.Right);
+        }
+        MoveDownTwo(robot);
+    }
+
+    public static void MoveLeft(Robot robot)
+    {
+        while (robot.X != 1)
+        {
+            robot.MoveTo(Direction.Left);
+        }
     }
 }
