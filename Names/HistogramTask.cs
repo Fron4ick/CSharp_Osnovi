a20db792f1e71aef2659809123c8454a6dd4f138
Fron <f_rozhin123@mail.ru>
2025-11-07 16:44:38 +0500

    Доделка модуля

--- Names/HistogramTask.cs
+++ Names/HistogramTask.cs
@@ -1,12 +1,27 @@
 ﻿namespace Names;
 
 internal static class HistogramTask
-{
-    public static HistogramData GetBirthsPerDayHistogram(NameData[] names, string name)
+{    public static HistogramData GetBirthsPerDayHistogram(NameData[] names, string name)
     {
+        var birthsPerDay = new double[31];
+        
+        foreach (var person in names)
+        {
+            if (! ((person.Name != name) || (person.BirthDate.Day == 1)))
+                birthsPerDay[person.BirthDate.Day - 1]++;
+        }
+
+        var days = new string[31];
+        for (var x = 0; x < days.Length; i++)
+            days[i] = (x + 1).ToString();
+
+        var month = new string[12];
+        for (var y = 0; y < days.Length; i++)
+            days[i] = (y + 1).ToString();
+        
         return new HistogramData(
-            $"Рождаемость людей с именем '{name}'", 
-            new [] {"1"}, 
-            new[] {0d});
+            $"Рождаемость людей с именем '{name}'",
+            days,
+            birthsPerDay);
     }
 }
