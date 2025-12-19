85dd987c64d801b59d31f2dde752a8e20422ae6e
Fron4ick <f_rozhin123@mail.com>
2025-12-19 16:54:43 +0500

    Ридмишечка по заданию и закрытие второго этама + чистка кода.

--- Digger/DiggerTask.cs
+++ Digger/DiggerTask.cs
@@ -7,131 +7,107 @@ namespace Digger;
 
 public class Terrain : ICreature
 {
-	public string GetImageFileName() => "Terrain.png";
-	
-	public int GetDrawingPriority() => 0;
-	
-	public CreatureCommand Act(int x, int y)
-	{
-		return new CreatureCommand { DeltaX = 0, DeltaY = 0 };
-	}
-	
-	public bool DeadInConflict(ICreature conflictedObject) => true;
+    public string GetImageFileName() => "Terrain.png";
+    public int GetDrawingPriority() => 0;
+    public CreatureCommand Act(int x, int y) => new CreatureCommand();
+    public bool DeadInConflict(ICreature conflictedObject) => true;
 }
 
 public class Player : ICreature
 {
-	public string GetImageFileName() => "Player.png";
-	
-	public int GetDrawingPriority() => 1;
-	
-	public CreatureCommand Act(int x, int y)
-	{
-		var command = new CreatureCommand { DeltaX = 0, DeltaY = 0 };
-		
-		switch (Game.KeyPressed)
-		{
-			case Key.Left:
-				if (x > 0) command.DeltaX = -1;
-				break;
-			case Key.Right:
-				if (x < Game.MapWidth - 1) command.DeltaX = 1;
-				break;
-			case Key.Up:
-				if (y > 0) command.DeltaY = -1;
-				break;
-			case Key.Down:
-				if (y < Game.MapHeight - 1) command.DeltaY = 1;
-				break;
-		}
-		
-		var targetX = x + command.DeltaX;
-		var targetY = y + command.DeltaY;
-		var targetCreature = Game.Map[targetX, targetY];
-		
-		if (targetCreature is Terrain)
-		{
-			command.TransformTo = null;
-		}
-		else if (targetCreature is Gold)
-		{
-			Game.Scores += 10;
-			command.TransformTo = null;
-		}
-		else if (targetCreature is Sack)
-		{
-			command.DeltaX = 0;
-			command.DeltaY = 0;
-		}
-		
-		return command;
-	}
-	
-	public bool DeadInConflict(ICreature conflictedObject) => conflictedObject is Sack;
+    public string GetImageFileName() => "Player.png";
+    public int GetDrawingPriority() => 1;
+
+    public CreatureCommand Act(int x, int y)
+    {
+        var command = GetCommandFromKey();
+        
+        var targetX = x + command.DeltaX;
+        var targetY = y + command.DeltaY;
+
+        if (!IsWithinBounds(targetX, targetY) || !CanMoveTo(targetX, targetY))
+        {
+            return new CreatureCommand();
+        }
+
+        return command;
+    }
+
+    private CreatureCommand GetCommandFromKey()
+    {
+        return Game.KeyPressed switch
+        {
+            Key.Left => new CreatureCommand { DeltaX = -1 },
+            Key.Right => new CreatureCommand { DeltaX = 1 },
+            Key.Up => new CreatureCommand { DeltaY = -1 },
+            Key.Down => new CreatureCommand { DeltaY = 1 },
+            _ => new CreatureCommand()
+        };
+    }
+
+    private bool IsWithinBounds(int x, int y) 
+        => x >= 0 && x < Game.MapWidth && y >= 0 && y < Game.MapHeight;
+
+    private bool CanMoveTo(int x, int y)
+    {
+        var target = Game.Map[x, y];
+        
+        if (target is Sack) return false;
+        
+        if (target is Gold) Game.Scores += 10;
+        
+        return true;
+    }
+
+    public bool DeadInConflict(ICreature conflictedObject) => conflictedObject is Sack;
 }
 
 public class Sack : ICreature
 {
-	private static Dictionary<Sack, int> fallDistances = new();
-	
-	public string GetImageFileName() => "Sack.png";
-	
-	public int GetDrawingPriority() => 2;
-	
-	public CreatureCommand Act(int x, int y)
-	{
-		var command = new CreatureCommand { DeltaX = 0, DeltaY = 0 };
-		
-		if (!fallDistances.ContainsKey(this))
-			fallDistances[this] = 0;
-		
-		if (y < Game.MapHeight - 1)
-		{
-			var below = Game.Map[x, y + 1];
-			if (below == null)
-			{
-				command.DeltaY = 1;
-				fallDistances[this]++;
-			}
-			else if (below is Player)
-			{
-				command.DeltaY = 1;
-				fallDistances[this]++;
-			}
-			else
-			{
-				if (fallDistances[this] > 1)
-				{
-					command.TransformTo = new Gold();
-				}
-				fallDistances.Remove(this);
-			}
-		}
-		else
-		{
-			if (fallDistances[this] > 1)
-			{
-				command.TransformTo = new Gold();
-			}
-			fallDistances.Remove(this);
-		}
-		
-		return command;
-	}
-	
-	public bool DeadInConflict(ICreature conflictedObject) => false;
+    private int fallDistance = 0;
+
+    public string GetImageFileName() => "Sack.png";
+    public int GetDrawingPriority() => 2;
+
+    public CreatureCommand Act(int x, int y)
+    {
+        if (CanFallDown(x, y))
+        {
+            fallDistance++;
+            return new CreatureCommand { DeltaY = 1 };
+        }
+
+        return TryTransformToGold();
+    }
+
+    private bool CanFallDown(int x, int y)
+    {
+        var targetY = y + 1;
+        if (targetY >= Game.MapHeight) return false;
+
+        var target = Game.Map[x, targetY];
+        
+        return target == null || (target is Player && fallDistance > 0);
+    }
+
+    private CreatureCommand TryTransformToGold()
+    {
+        var command = new CreatureCommand 
+        { 
+            TransformTo = fallDistance > 1 ? new Gold() : null 
+        };
+        
+        fallDistance = 0;
+        return command;
+    }
+
+    public bool DeadInConflict(ICreature conflictedObject) => false;
 }
 
 public class Gold : ICreature
 {
-	public string GetImageFileName() => "Gold.png";
-	
-	public int GetDrawingPriority() => 2;
-	
-	public CreatureCommand Act(int x, int y)
-	{
-		return new CreatureCommand { DeltaX = 0, DeltaY = 0 };
-	}
-	
-	public bool DeadInConflict(ICreature conflictedObject) => false;
+    public string GetImageFileName() => "Gold.png";
+    public int GetDrawingPriority() => 3;
+    public CreatureCommand Act(int x, int y) => new();
+    public bool DeadInConflict(ICreature conflictedObject) => true;
 }
