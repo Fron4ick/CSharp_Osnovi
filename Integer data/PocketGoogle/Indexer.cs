9ea24d5a600e920c82ed7777cc6f99bd588fb5c7
Fron4ick <f_rozhin123@mail.com>
2025-12-20 11:58:18 +0500

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch 14--ulearn-Integer-data
     Your branch is up to date with 'origin/14--ulearn-Integer-data'.
    
     Changes to be committed:
    new file:   Integer data/HotelAccounting/AccountingModel.cs
    new file:   Integer data/HotelAccounting/HotelAccounting.csproj
    new file:   Integer data/HotelAccounting/ModelBase.cs
    new file:   Integer data/HotelAccounting/UI/App.axaml
    new file:   Integer data/HotelAccounting/UI/App.axaml.cs
    new file:   Integer data/HotelAccounting/UI/MainWindow.axaml
    new file:   Integer data/HotelAccounting/UI/MainWindow.axaml.cs
    new file:   Integer data/HotelAccounting/UI/Program.cs
    new file:   Integer data/PocketGoogle/IIndexer.cs
    new file:   Integer data/PocketGoogle/Indexer.cs
    new file:   Integer data/PocketGoogle/PocketGoogle.csproj
    new file:   Integer data/PocketGoogle/Texts/0.txt
    new file:   Integer data/PocketGoogle/Texts/1.txt
    new file:   Integer data/PocketGoogle/Texts/2.txt
    new file:   Integer data/PocketGoogle/Texts/4.txt
    new file:   Integer data/PocketGoogle/UI/App.axaml
    new file:   Integer data/PocketGoogle/UI/App.axaml.cs
    new file:   Integer data/PocketGoogle/UI/MainWindow.axaml
    new file:   Integer data/PocketGoogle/UI/MainWindow.axaml.cs
    new file:   Integer data/PocketGoogle/UI/Program.cs
    modified:   Integer data/Vector.cs

---
+++ Integer data/PocketGoogle/Indexer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PocketGoogle;
+
+public class Indexer : IIndexer
+{
+    private readonly Dictionary<string, Dictionary<int, List<int>>> data = 
+        new Dictionary<string, Dictionary<int, List<int>>>();
+    
+    private static readonly char[] separators = { ' ', '.', ',', '!', '?', ':', '-', '\r', '\n' };
+
+    public void Add(int id, string documentText)
+    {
+        int currentPos = 0;
+        string[] words = documentText.Split(separators);
+
+        foreach (var word in words)
+        {
+            if (!string.IsNullOrEmpty(word))
+            {
+                if (!data.ContainsKey(word))
+                    data[word] = new Dictionary<int, List<int>>();
+
+                if (!data[word].ContainsKey(id))
+                    data[word][id] = new List<int>();
+
+                data[word][id].Add(currentPos);
+            }
+            currentPos += word.Length + 1;
+        }
+    }
+
+    public List<int> GetIds(string word)
+    {
+        if (data.ContainsKey(word))
+        {
+            return data[word].Keys.ToList();
+        }
+        return new List<int>();
+    }
+
+    public List<int> GetPositions(int id, string word)
+    {
+        if (data.ContainsKey(word) && data[word].ContainsKey(id))
+        {
+            return data[word][id];
+        }
+        return new List<int>();
+    }
+
+    public void Remove(int id)
+    {
+        foreach (var wordEntry in data.Values)
+        {
+            wordEntry.Remove(id);
+        }
+    }
+}
--- Integer data/Vector.cs
+++ Integer data/Vector.cs
@@ -1,85 +1,29 @@
-namespace Geometry
+namespace ReadOnlyVector
 {
-    public class Vector
+    public class ReadOnlyVector
     {
-        public double X;
-        public double Y;
+        public readonly double X;
+        public readonly double Y;
 
-        public double GetLength()
+        public ReadOnlyVector(double x, double y)
         {
-            return Geometry.GetLength(this);
+            X = x;
+            Y = y;
         }
 
-        public Vector Add(Vector other)
+        public ReadOnlyVector Add(ReadOnlyVector other)
         {
-            return Geometry.Add(this, other);
+            return new ReadOnlyVector(X + other.X, Y + other.Y);
+		}
+
+        public ReadOnlyVector WithX(double x)
+        {
+            return new ReadOnlyVector(x, Y);
         }
 
-        public bool Belongs(Segment segment)
+        public ReadOnlyVector WithY(double y)
         {
-            return Geometry.IsVectorInSegment(this, segment);
-        }
-    }
-
-    public class Segment
-    {
-        public Vector Begin;
-        public Vector End;
-
-        public double GetLength()
-        {
-            return Geometry.GetLength(this);
-        }
-
-        public bool Contains(Vector point)
-        {
-            return Geometry.IsVectorInSegment(point, this);
-        }
-    }
-
-    public class Geometry
-    {
-        public static double GetLength(Vector vector)
-        {
-            return System.Math.Sqrt(vector.X * vector.X + vector.Y * vector.Y);
-        }
-
-        public static double GetLength(Segment segment)
-        {
-            Vector diff = new Vector { X = segment.End.X - segment.Begin.X, Y = segment.End.Y - segment.Begin.Y };
-            return GetLength(diff);
-        }
-
-        public static Vector Add(Vector vector1, Vector vector2)
-        {
-            return new Vector { X = vector1.X + vector2.X, Y = vector1.Y + vector2.Y };
-        }
-
-        public static bool IsVectorInSegment(Vector point, Segment segment)
-        {
-            double minX = System.Math.Min(segment.Begin.X, segment.End.X);
-            double maxX = System.Math.Max(segment.Begin.X, segment.End.X);
-            double minY = System.Math.Min(segment.Begin.Y, segment.End.Y);
-            double maxY = System.Math.Max(segment.Begin.Y, segment.End.Y);
-
-            if (point.X < minX || point.X > maxX || point.Y < minY || point.Y > maxY)
-            {
-                return false;
-            }
-
-            Vector segmentVec = new Vector { X = segment.End.X - segment.Begin.X, Y = segment.End.Y - segment.Begin.Y };
-            Vector pointVec = new Vector { X = point.X - segment.Begin.X, Y = point.Y - segment.Begin.Y };
-
-            double segmentLengthSq = segmentVec.X * segmentVec.X + segmentVec.Y * segmentVec.Y;
-            double dotProduct = pointVec.X * segmentVec.X + pointVec.Y * segmentVec.Y;
-
-            if (dotProduct < 0 || dotProduct > segmentLengthSq)
-            {
-                return false;
-            }
-
-            double crossProduct = pointVec.X * segmentVec.Y - pointVec.Y * segmentVec.X;
-            return System.Math.Abs(crossProduct) < 1e-10;
+            return new ReadOnlyVector(X, y);
         }
     }
 }
